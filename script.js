5a6b8115fa63ecf069a05feac9f6028e131c5a51
kmaimn <kmai.mn@gmail.com>
2016-07-13 13:33:38 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var avengersMovie = {
+	name: "Avengers",
+	runtimeMins: 143,
+	releaseYear: "2012",
+	director: "Joss Whedon"
+};
+//Could I have left of Mins in my property name or is it good practice to include the unit of measure?
+//I still wanted to keep 2012 as a string since I don't see if as a numerical value
+
+console.log(avengersMovie);
