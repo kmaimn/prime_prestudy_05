c4518940ebb7fc137282e18accdf8f81edc34927
kmaimn <kmai.mn@gmail.com>
2016-10-18 08:59:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,5 @@ var avengersMovie = {
 	releaseYear: "2012",
 	director: "Joss Whedon"
 };
-//Could I have left of Mins in my property name or is it good practice to include the unit of measure?
-//I still wanted to keep 2012 as a string since I don't see if as a numerical value
 
 console.log(avengersMovie);
